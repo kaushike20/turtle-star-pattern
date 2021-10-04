8ccb3b687bb6a33cd3ba5b092a147319a0091b03
Abhiraj Agrawal <61726538+Devil-aa@users.noreply.github.com>
2021-10-04 12:30:12 +0530

    Create star.py

---
+++ star.py
@@ -0,0 +1,42 @@
+import turtle
+import time
+
+col=('red','yellow','green','cyan','pink','white','violet','orange','indigo')
+
+t=turtle.Turtle()
+t.width(3)
+screen=turtle.Screen()
+screen.bgcolor('black')
+t.speed(25)
+
+x=1
+
+for i in range(35):
+    for j in range(4):
+        t.color(col[(0+i+j)%9])
+        t.fd(x)
+        t.lt(45)
+        t.color(col[(1+i+j)%9])
+        t.fd(x)
+        t.rt(90)
+        t.color(col[(2+i+j)%9])
+        t.fd(x)
+        t.lt(45)
+        t.color(col[(3+i+j)%9])
+        t.fd(x)
+        t.rt(90)
+    t.lt(135)
+    t.up()
+    t.fd(10)
+    t.down()
+    t.rt(135)
+    x=x+4
+
+t.up()
+t.goto(-425,-80)
+t.down()
+t.pencolor('blue')
+t.write("THANK YOU!",align='left',font=("Comic Sans MS",100,'bold'))
+t.hideturtle()
+
+time.sleep(10)
